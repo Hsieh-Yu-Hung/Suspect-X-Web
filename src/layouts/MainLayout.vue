10d2e4270a5314ac5a88827d9ac4451909625748
Hsieh-Yu-Hung <yuhunghsieh@accuinbio.com>
2025-01-02 12:21:25 +0800

    Update Index Page

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,106 +1,71 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+  <q-layout view="hHh lpR lFr">
 
+    <!-- Layout Header -->
+    <q-header reveal elevated class="bg-indigo-2 text-white" height-hint="98">
+
+      <!-- 標題列 -->
+      <q-toolbar>
+
+        <!-- 左邊選單按鈕 -->
+        <q-btn flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <!-- 標題 -->
         <q-toolbar-title>
-          Quasar App
+          <div style="display: flex; flex-direction: row; align-items: center;">
+            <!-- logo -->
+            <div class="logo" style="height: 35px; margin-inline: 10px;">
+              <img src="/icon.png" style="height: 100%;">
+            </div>
+            <!-- 標題文字 -->
+            <div style="height: 40px; margin-inline: 10px; display: flex; flex-direction: row; align-items: center;">
+              <span style="font-size: 1.4em; font-weight: bold; color: black;">ACCUiNspection
+                <span style="font-size: 0.4em; font-weight: bold; color: slategray; margin-inline: 5px;">WEB Service</span>
+              </span>
+            </div>
+          </div>
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <!-- 右邊選單按鈕 -->
+        <q-btn dense flat icon="group" label="Show Account" @click="toggleRightDrawer" color="black"/>
       </q-toolbar>
+
+      <!-- 頁籤按鈕 -->
+      <q-tabs align="right" class="text-blue-grey-8">
+        <q-route-tab to="/page-import" label="Import" />
+        <q-route-tab to="/page-analysis" label="Analysis" />
+        <q-route-tab to="/page-export" label="Export" />
+      </q-tabs>
+
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+    <q-drawer v-model="leftDrawerOpen" side="left" overlay bordered>
+      <!-- drawer content -->
+    </q-drawer>
 
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+    <q-drawer v-model="rightDrawerOpen" side="right" overlay behavior="mobile" bordered>
+      <!-- drawer content -->
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
+
   </q-layout>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
-
-defineOptions({
-  name: 'MainLayout'
-})
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
 
 const leftDrawerOpen = ref(false)
+const rightDrawerOpen = ref(false)
 
-function toggleLeftDrawer () {
+const toggleLeftDrawer = () => {
   leftDrawerOpen.value = !leftDrawerOpen.value
 }
+
+const toggleRightDrawer = () => {
+  rightDrawerOpen.value = !rightDrawerOpen.value
+}
+
 </script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,19 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; cursor: pointer;" @click="login">
+      <!-- Logo -->
+      <img
+        alt="ACCUiNspection logo"
+        src="/logo.png"
+        height="40px"
+      >
+      <!-- Login button -->
+      <div style="width: 100%; display: flex; flex-direction: row; align-items: center; justify-content: space-evenly; margin-top: 1.5em;">
+        <q-icon class="login-hint" name="east" />
+        <span class="login-hint">Click to login</span>
+        <q-icon class="login-hint" name="west" />
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -12,4 +21,19 @@
 defineOptions({
   name: 'IndexPage'
 });
+
+/* functions */
+
+// Login function
+const login = () => {
+  console.log('Login');
+}
+
 </script>
+
+<style scoped>
+.login-hint {
+  font-size: 14px;
+  color: silver;
+}
+</style>
